8a840e45f4c4930d429bd4885272621a11adad95
Francesco Ganora <francescoganora@Francescos-MBP.station>
2016-07-22 17:33:16 +0200

    Added instance configuration logic based on nconf package

--- int-node.js
+++ int-node.js
@@ -22,17 +22,59 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 "use strict";
 
-// print env
-const environment = process.env.NODE_ENV || 'development'
-console.log("Environment = " + environment);
+const fs = require('fs');
+const nconf = require('nconf');
 
-// get options
-var  options = require('./lib/options')(process.argv);
 
-console.log("2. command-line options: " + JSON.stringify(options));
+
+// load instance configuration and merge command line options
+var instanceConfigFile = __dirname + '/config/integration-node-config.json';  // default config file
+
+// config file specified
+if (process.argv[2] && !process.argv[2].startsWith('--')) {
+  try {
+        fs.accessSync(process.argv[2]);
+  }
+  catch (e) {
+    console.error('Specified instance configuration file ' + process.argv[2] + ' not found or not readable. Integration-Node instance must abort.' );
+    console.error('Original error is:');
+    console.error(e);
+    process.exit(1);
+  }
+  instanceConfigFile = process.argv[2];
+}
+
+// validate confif file
+try {
+  let configFileContents = fs.readFileSync(instanceConfigFile);
+  JSON.parse(configFileContents);
+}
+catch (e) {
+  console.error('Instance configuration file ' + instanceConfigFile + ' is not a valid JSON file. Integration-Node instance must abort.' );
+  console.error('Original error is:');
+  console.error(e);
+  process.exit(1);
+
+}
+
+nconf.add('INSTANCE', { type: 'file', file: instanceConfigFile });
+
+var instanceConfiguration = nconf.get();
+var cmdLineOptions = require('./lib/options')(process.argv);
+for ( var opt in cmdLineOptions) {
+  instanceConfiguration[opt] = cmdLineOptions[opt];
+}
+
+console.log("Command-line options: " + JSON.stringify(instanceConfiguration));
+
+// save options (TEST)
+// nconf.save(function (err) {});
+
+
+
 
 // startIntegration-Node  controller
-var  controller = require('./lib/controller')(options, __dirname);
+var  controller = require('./lib/controller')(instanceConfiguration, __dirname);
 
 //
 // graceful shutdown
